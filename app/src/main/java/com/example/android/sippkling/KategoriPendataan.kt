e1aaf53ed40c2b3af4d67bf4b1b662ec76847b36
dianaambar <dianaambar745@gmail.com>
2019-01-30 00:42:37 +0700

    kategori pendataan

---
+++ app/src/main/java/com/example/android/sippkling/KategoriPendataan.kt
@@ -0,0 +1,12 @@
+package com.example.android.sippkling
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class KategoriPendataan : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.Layout.activity_kategori_pendataan)
+    }
+}
